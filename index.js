8b95e6a4c23c2388c40b4c5f4709c09d4a1babfa
Kizhakkekara-ext, Sujith (GIT-APGK) <Sujith.Kizhakkekara-ext@bshg.com>
2021-04-26 17:01:00 +0530

    Added required Comments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Importing packages
 const express = require("express");
 const Keycloak = require("keycloak-connect");
 var session = require("express-session");
@@ -17,11 +18,15 @@ const keycloakConf = {
   "confidential-port": 0,
 };
 
+// Initializing Keycloak with memory store and Config.
 const kc = new Keycloak({ store: memoryStore }, keycloakConf);
 
 // Middle-wares
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+// To set the session to prevent Keycloak middleware from throwing error.
+// Always use this before initializing Keycloak middleware.
 app.use(
   session({
     secret: "some secret",
@@ -30,6 +35,8 @@ app.use(
     store: memoryStore,
   })
 );
+
+// Keycloak middleware 
 app.use(
   kc.middleware({
     logout: "/logout",
@@ -37,21 +44,26 @@ app.use(
   })
 );
 
-
+// The "/" route is unprotected. So, we can access without logging in. 
 app.get("/", (req, res) => {
   res.render("unprotected", { logStatus: "out"});
 });
 
+// We used kc.protect() to redirect to KeyCloak login page.
+// Once the user is authenticated it will redirect to "/home" route. 
 app.get("/login", kc.protect(), (req, res) => {
   res.redirect("/home");
 });
 
+// We have used kc.protect() to protect this route "/home" 
 app.get("/home", kc.protect(), (req, res, next) => {
   res.render("home", { logStatus: "in" });
 });
 
+// To Logout the user with keycloak using middelware defined above 
 app.get("/logout", (req, res, next) => {
   res.send("Logged Out");
 });
 
+// Starting server at port 3000
 app.listen(3000, () => console.log("Server Started at 3000....."));
